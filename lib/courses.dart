f6a61d37aed31c2ed40f8e343ef309fa5a9f6879
gaurav <270mehta@gmail.com>
2021-01-09 23:01:21 +0530

    minor fixes

--- lib/courses.dart
+++ lib/courses.dart
@@ -7,67 +7,78 @@ class FreeCourses extends StatelessWidget {
     return Scaffold(
         body: Stack(
       children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 50,
-            ),
-            Text('Individual Courses',
-                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20)),
-            SizedBox(
-              height: 25,
-            ),
-            Text(
-              'Choose the one which suits you best.',
-              style: TextStyle(fontWeight: FontWeight.w400),
-            ),
-            SizedBox(
-              height: 75,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(20),
-                  width: MediaQuery.of(context).size.width / 4,
-                  color: Colors.blue,
-                  child: Column(
-                    children: [
-                      Text(
-                        'ML',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w800, fontSize: 20),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                          'Understanding data, ML techniques, Model Tuning, XGboost.'),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        'Community support: Yes \n Duration: 4 Weeks \n Projects: 5',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w600, fontSize: 15),
-                      )
-                    ],
+        SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 100,
+              ),
+              Text('Individual Courses',
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20)),
+              SizedBox(
+                height: 25,
+              ),
+              Text(
+                'Choose the one which suits you best.',
+                style: TextStyle(fontWeight: FontWeight.w400),
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    width: MediaQuery.of(context).size.width / 4,
+                    color: Colors.blueGrey[50],
+                    child: Column(
+                      children: [
+                        Text(
+                          'ML',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w800, fontSize: 20),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          'Understanding data, ML techniques, Model Tuning, XGboost.',
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          'Community support: Yes \n Duration: 4 Weeks \n Projects: 5',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600, fontSize: 15),
+                        ),
+                        Text(
+                          'Free',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w800,
+                              fontSize: 20,
+                              color: Colors.blue),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Container(
-                  height: 200,
-                  width: MediaQuery.of(context).size.width / 4,
-                  color: Colors.red,
-                ),
-                Container(
-                  height: 200,
-                  width: MediaQuery.of(context).size.width / 4,
-                  color: Colors.green,
-                )
-              ],
-            )
-          ],
+                  Container(
+                    height: 200,
+                    width: MediaQuery.of(context).size.width / 4,
+                    color: Colors.blueGrey[50],
+                  ),
+                  Container(
+                    height: 200,
+                    width: MediaQuery.of(context).size.width / 4,
+                    color: Colors.blueGrey[50],
+                  )
+                ],
+              )
+            ],
+          ),
         ),
         topbar(),
       ],
