03b59d9a5532a5fb0eab27247c45837d244cbe56
gaurav <270mehta@gmail.com>
2021-01-11 08:11:34 +0530

    Minor Fixes

--- lib/Join Us/Desktop JoinUs.dart
+++ lib/Join Us/Desktop JoinUs.dart
@@ -7,73 +7,191 @@ class DesktopJoin extends StatelessWidget {
     return Scaffold(
       body: Stack(children: [
         SingleChildScrollView(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 125,
-            ),
-            Text('BE A PART OF OUR JOURNEY',
-                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 30)),
-            SizedBox(
-              height: 15,
-            ),
-            Text(
-              'Join us either as a Community contributor or as an Intern.',
-              style: TextStyle(fontWeight: FontWeight.w400),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                RaisedButton(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          SizedBox(
+            height: 125,
+          ),
+          Text('BE A PART OF OUR JOURNEY',
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 30)),
+          SizedBox(
+            height: 15,
+          ),
+          Text(
+            'Join us either as a Community contributor or as an Intern.',
+            style: TextStyle(fontWeight: FontWeight.w400),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RaisedButton(
+                color: const Color(0xff2821b5),
+                textColor: Colors.white,
+                padding: EdgeInsets.fromLTRB(30, 20, 30, 20),
+                onPressed: () {},
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(1)),
+                child: Container(
                   color: const Color(0xff2821b5),
-                  textColor: Colors.white,
-                  padding: EdgeInsets.fromLTRB(30, 20, 30, 20),
-                  onPressed: () {},
-                  shape: new RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(1)),
-                  child: Container(
-                    color: const Color(0xff2821b5),
-                    child: Text(
-                      'Contributor',
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w500,
-                      ),
+                  child: Text(
+                    'Contributor',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
                     ),
                   ),
                 ),
-                SizedBox(
-                  width: 50,
-                ),
-                RaisedButton(
+              ),
+              SizedBox(
+                width: 50,
+              ),
+              RaisedButton(
+                color: const Color(0xff2821b5),
+                textColor: Colors.white,
+                padding: EdgeInsets.fromLTRB(30, 20, 30, 20),
+                onPressed: () {},
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(1)),
+                child: Container(
                   color: const Color(0xff2821b5),
-                  textColor: Colors.white,
-                  padding: EdgeInsets.fromLTRB(30, 20, 30, 20),
-                  onPressed: () {},
-                  shape: new RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(1)),
-                  child: Container(
-                    color: const Color(0xff2821b5),
-                    child: Text(
-                      'Intern',
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w500,
-                      ),
+                  child: Text(
+                    'Intern',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
                     ),
                   ),
                 ),
-              ],
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Divider(),
+          Text('Contributor Program',
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 30)),
+          SizedBox(
+            height: 25,
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            // 1st option
+
+            Container(
+              padding: EdgeInsets.all(20),
+              width: MediaQuery.of(context).size.width / 4,
+              color: Colors.blueGrey[50],
+              child: Column(
+                children: [
+                  Text(
+                    'CAMPUS\nAMBASSADOR',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
+                  ),
+                  Divider(color: Colors.blue),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Join our community\nBe our face in your campus ',
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Duration: 4 Weeks \n\n Perks : Certificate',
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 15),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  FlatButton(
+                      onPressed: () {},
+                      shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(10)),
+                      child: Container(
+                          padding: EdgeInsets.fromLTRB(50, 10, 50, 10),
+                          color: Colors.green,
+                          child: Text(
+                            'Join Now',
+                            style: TextStyle(color: Colors.white),
+                          )))
+                ],
+              ),
             ),
-            Divider(),
-            Text('Contributor',
-                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 30)),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-              // 1st course
+
+            //2nd option
+
+            Container(
+              padding: EdgeInsets.all(20),
+              width: MediaQuery.of(context).size.width / 4,
+              color: Colors.blueGrey[50],
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'COMMUNITY CONTRIBUTOR',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
+                  ),
+                  Divider(color: Colors.blue),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Contribute to our community with the knowledge you have.',
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Duration: Flexible \n\n Perks: Certificate of Contribution',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 15),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  FlatButton(
+                      onPressed: () {},
+                      shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(10)),
+                      child: Container(
+                          padding: EdgeInsets.fromLTRB(50, 10, 50, 10),
+                          color: Colors.green,
+                          child: Text(
+                            'Join Now',
+                            style: TextStyle(color: Colors.white),
+                          )))
+                ],
+              ),
+            ),
+          ]),
+          SizedBox(
+            height: 25,
+          ),
+          Divider(),
+          SizedBox(
+            height: 25,
+          ),
+//
+//
+//
+
+          Text('Internship',
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 30)),
+          SizedBox(
+            height: 25,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              // 1st option
 
               Container(
                 padding: EdgeInsets.all(20),
@@ -82,7 +200,7 @@ class DesktopJoin extends StatelessWidget {
                 child: Column(
                   children: [
                     Text(
-                      'CAMPUS AMBASSADOR',
+                      'Web Development',
                       style:
                           TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
                     ),
@@ -91,14 +209,15 @@ class DesktopJoin extends StatelessWidget {
                       height: 10,
                     ),
                     Text(
-                      'Join our community\nBe our face in your campus ',
+                      'Work on our website with core team.',
                       textAlign: TextAlign.center,
                     ),
                     SizedBox(
                       height: 20,
                     ),
                     Text(
-                      'Duration: 4 Weeks \n\n Perks : Certificate',
+                      'Duration: 4 Weeks \n\n Perks : Internship Certificate, LOR, Other Perks*',
+                      textAlign: TextAlign.center,
                       style:
                           TextStyle(fontWeight: FontWeight.w600, fontSize: 15),
                     ),
@@ -120,7 +239,7 @@ class DesktopJoin extends StatelessWidget {
                 ),
               ),
 
-              //2nd course
+              //2nd option
 
               Container(
                 padding: EdgeInsets.all(20),
@@ -130,7 +249,8 @@ class DesktopJoin extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Text(
-                      'COMMUNITY CONTRIBUTOR',
+                      'Content Creator\nML',
+                      textAlign: TextAlign.center,
                       style:
                           TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
                     ),
@@ -139,14 +259,15 @@ class DesktopJoin extends StatelessWidget {
                       height: 10,
                     ),
                     Text(
-                      'Contribute to our community with the knowledge you have.',
+                      'Work on write-ups and projects.',
                       textAlign: TextAlign.center,
                     ),
                     SizedBox(
                       height: 20,
                     ),
                     Text(
-                      'Duration: Flexible \n\n Perks: Certificate of Contribution',
+                      'Duration: Flexible \n\n Perks: Internship Certificate, LOR and Other Perks*',
+                      textAlign: TextAlign.center,
                       style:
                           TextStyle(fontWeight: FontWeight.w600, fontSize: 15),
                     ),
@@ -167,9 +288,9 @@ class DesktopJoin extends StatelessWidget {
                   ],
                 ),
               ),
-            ])
-          ],
-        )),
+            ],
+          )
+        ])),
         Topbar()
       ]),
     );
